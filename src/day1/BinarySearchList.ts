9283afe60a7b0ece555b8e2c5495614320658a42
Zack Wallace <zwallace0790@gmail.com>
2023-09-22 16:09:38 -0700

    Implemented binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,43 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
+/* 
+ * Given a sorted array, split the array in half.
+ * If needle is less than the value at 1/2 the array, use the lower half. Else, use the upper half.
+ * Repeat until value is found, or until the array can no longer be cut in half.
+ * 
+ * Complexity: O(logN)
+ * 
+ * Pseudo-code:
+ * 
+ * function search(arr. lo, hi, needle):
+ *    midpoint = floor(lo + (hi - lo) / 2)
+ *    value = arr[midpoint]
+ *
+ *    while lo < hi:
+ *        if value = needle:
+ *            return true
+ *         else if value > needle:
+ *             hi = midpoint
+ *         else:
+ *             lo = midpoint + 1
+ *
+ *    return false
+*/
 
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0
+    let hi = haystack.length
+    
+    while (lo < hi) {
+        const midpoint = Math.floor(lo + (hi - lo) / 2)
+        const value = haystack[midpoint]
+
+        if (value === needle) {
+            return true
+        } else if (value > needle) {
+            hi = midpoint
+        } else {
+            lo = midpoint + 1
+         }
+    }
+
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,38 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+/*
+ * Given two crystal balls that will break if dropped from high enough
+ * distance, determine the exact spot in which it will break in the most
+ * optimized way.
+ * 
+ *** Approach notes:
+ * 
+ * Use binary search! In this case, we will jump sqrt(n) instead of n/2.
+ * Complexity: O(sqrt(n))
+*/
 
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length))
+
+    // First crystal ball...
+    // Jump forward sqrt(n)
+    let i = jumpAmount
+
+    // Check for breaks, jumping sqrt(n) each time
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    // We found a break! Walk back sqrt(n)
+    // Second crystal ball...
+    i -= jumpAmount
+    
+    // Lineary walk forward until break is found
+    for (let j = 0; j < jumpAmount && i < breaks.length; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
