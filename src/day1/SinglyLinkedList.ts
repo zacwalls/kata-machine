c5cd77fc07d0b4585eb057e9261ba88804618bc2
Zack Wallace <zwallace0790@gmail.com>
2023-09-26 21:28:23 -0700

    Taking notes on Linked lists + Queue

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,37 @@
+/*
+ * A singly linked list is a data structure composed of nodes that point to the next item.
+ * Each node contains a value, and a reference to the next node.
+ * A doubly linked list contains a previous pointer as well.
+ * 
+ */
+
 export default class SinglyLinkedList<T> {
     public length: number;
-
-    
 
     constructor() {
     }
 
     prepend(item: T): void {
 
-}
+    }
+
     insertAt(item: T, idx: number): void {
 
-}
+    }
+
     append(item: T): void {
 
-}
+    }
+
     remove(item: T): T | undefined {
 
-}
+    }
+
     get(idx: number): T | undefined {
 
-}
+    }
+
     removeAt(idx: number): T | undefined {
 
-}
+    }
 }
