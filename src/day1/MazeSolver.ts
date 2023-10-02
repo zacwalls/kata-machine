4e8e4a25b00fb1c07184fcf969a7069e123e91df
Zack Wallace <zwallace0790@gmail.com>
2023-10-02 15:48:22 -0700

    Implemented stack + maze solver

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,85 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+/**
+ * S =start
+ * E = end
+ * [
+ *   #####E#
+ *   #     #
+ *   #S#####
+ * ]
+ * 
+ * Base case:
+ *   1. Off the map
+ *   2. On a wall
+ *   3. Its the end
+ *   4. If we have seen it
+ */
 
+type Point = {
+    x: number,
+    y: number
+}
+
+// Differential directions
+const dir = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1]
+]
+
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]): boolean {
+
+    // off map
+    if (curr.x < 0 || curr.x >= maze[0].length || curr.y < 0 || curr.y >= maze.length) {
+        return false;
+    }
+
+    // on a wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    // at end
+    if (curr.x === end.x && curr.y === end.y) {
+        path.push(end);
+        return true;
+    }
+
+    // seen point
+    if (seen[curr.y][curr.x]) {
+        return false
+    }
+
+    // 3 recurse
+    // pre
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
+
+    // recurse
+    for (let i = 0; i < dir.length; ++i) {
+        const [x, y] = dir[i]
+        if (walk(maze, wall, {
+            x: curr.x + x,
+            y: curr.y + y
+        }, end, seen, path)) {
+            return true;
+        }
+    }
+
+    // post
+    path.pop();
+    return false;
+}
+
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
+
+    for (let i = 0; i < maze[0].length; ++i) {
+        seen.push(new Array(maze[0].length).fill(false))
+    }
+
+    walk(maze, wall, start, end, seen, path);
+
+    return path;
 }
